6d57c4afbb4efe4e4f4c3e4008f7fb526a49a473
Higor Rocha <you@example.com>
2021-07-24 10:14:12 -0300

    Completing the app

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -9,6 +10,62 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+
+  var _imageApp = AssetImage("images/default.png");
+  var _message = "Make Your Choice";
+
+  void _optionSelected(String choiceUser){
+
+    var option = ["paper", "rock", "scissors"];
+    var number = Random().nextInt(option.length);
+    var choiceApp = option[number];
+
+    //Show the image chosen by the App
+    switch(choiceApp){
+      case "paper":
+        setState(() {
+          this._imageApp = AssetImage("images/paper.png");
+        });
+        break;
+      case "rock":
+        setState(() {
+          this._imageApp = AssetImage("images/rock.png");
+        });
+        break;
+      case "scissors":
+        setState(() {
+          this._imageApp = AssetImage("images/scissors.png");
+        });
+        break;
+    }
+
+    //Validation of who won the game
+    if(
+    //User win if
+      (choiceUser == "rock" && choiceApp == "scissors") ||
+      (choiceUser == "scissors" && choiceApp == "paper") ||
+      (choiceUser == "paper" && choiceApp == "rock")
+    ){
+      setState(() {
+        this._message = "Congratulations !*_*! You WIN!";
+      });
+    }else if(
+    //App win if
+      (choiceApp == "rock" && choiceUser == "scissors") ||
+      (choiceApp == "scissors" && choiceUser == "paper") ||
+      (choiceApp == "paper" && choiceUser == "rock")
+    ){
+      setState(() {
+        this._message = "You Lose! :/";
+      });
+    }else{
+      setState(() {
+        this._message = "You Tied. Try Play Again. :)";
+      });
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,16 +86,11 @@ class _GameState extends State<Game> {
               ),
             ),
           ),
-          GestureDetector(
-            onTap: (){ print("One click in the image"); },
-            onDoubleTap: (){ print("Two clicks in the image"); },
-            onLongPress: (){ print("Long click in the image"); },
-            child: Image.asset("images/default.png"),
-          ),
+          Image(image: this._imageApp),
           Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
-              "Make Your Choice",
+              this._message,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 20,
@@ -49,9 +101,23 @@ class _GameState extends State<Game> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              GestureDetector(
+                onTap: () => _optionSelected("paper"),
+                child: Image.asset("images/paper.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => _optionSelected("rock") ,
+                child: Image.asset("images/rock.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => _optionSelected("scissors"),
+                child: Image.asset("images/scissors.png", height: 100,),
+              )
+              /*
               Image.asset("images/paper.png", height: 100),
               Image.asset("images/rock.png", height: 100),
               Image.asset("images/scissors.png", height: 100)
+              */
             ],
           )
         ],
